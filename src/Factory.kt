6ebe06c9a80cdf6facaa4517b3fd8495a6f01dbb
Tien Dung Pham <dungpham1218@gmail.com>
2020-05-26 15:42:27 +0700

    Factory Desgin sample

---
+++ src/Factory.kt
@@ -0,0 +1,37 @@
+// https://refactoring.guru/design-patterns/factory-method
+
+interface Car {
+    fun drive(): Unit
+}
+
+class NissanCar : Car {
+    override fun drive() {
+        print("Nissan engine start")
+    }
+}
+
+class MiniCooperCar : Car {
+    override fun drive() {
+        print("Mini Cooper engine start")
+    }
+}
+
+abstract class Factory {
+    abstract fun create(carType: String): Car
+}
+
+class CarFactory : Factory() {
+    override fun create(carType: String): Car {
+        return if (carType == "nissan") {
+            NissanCar()
+        } else {
+            MiniCooperCar()
+        }
+    }
+}
+
+fun main() {
+    val carFactory: CarFactory = CarFactory()
+    carFactory.create("nissan").drive()
+}
+
